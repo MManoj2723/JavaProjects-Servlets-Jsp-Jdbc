39cb8dc66cd63d36bc26659fc1da08bcf7c5c7d1
MManoj2723 <mmanoj2723@gmail.com>
2025-05-06 18:36:00 +0530

    Add Student Management project

---
+++ StudentManagement/src/main/java/test/DBInfo.java
@@ -0,0 +1,14 @@
+package test;
+
+public interface DBInfo {
+	public static final String 
+						driver="oracle.jdbc.driver.OracleDriver";
+	
+	public static final String 
+						url="jdbc:oracle:thin:@Localhost:1521:ORCL";
+	public static final String 
+						uname = "manoj";
+	public static final String
+						pswd ="manoj2723";
+	
+}
